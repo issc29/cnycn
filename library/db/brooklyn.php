a956e94e02b8052acc3c9f68cfc29ece77e05674
Isaac Cohen <issc29@aol.com>
2014-09-03 13:04:44 -0400

    cleaned up files. prepping for integration

---
+++ library/db/brooklyn.php
@@ -0,0 +1,20 @@
+<?php
+$dsn = 'mysql:dbname=boroughs;host=127.0.0.1';
+$user = 'root';
+$password = 'ffgdb2014!';
+try
+{
+    $dbh = new PDO($dsn, $user, $password);
+    $input_street = $_GET['street_names'];
+    $qry = $dbh->prepare('SELECT year_built from brooklyn where street_names like ?');
+     $qry->execute(array($input_street));
+    $result = $qry->fetchColumn();
+    echo $result; 
+   
+    header('HTTP/1.1 201 Created', true, 201);
+}
+catch(PDOException $e)
+{
+    header('HTTP/1.1 500 Internal Server Error');
+}
+?>
